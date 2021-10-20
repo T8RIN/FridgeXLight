027d302ee9b596d77d6653dd9f6e892c0ede931f
T8RIN <oolbp@bk.ru>
2021-10-20 23:13:53 +0300

    code cleanup

--- app/src/main/java/com/progix/fridgex/light/adapter/DailyAdapter.kt
+++ app/src/main/java/com/progix/fridgex/light/adapter/DailyAdapter.kt
@@ -83,7 +83,6 @@ class DailyAdapter(
             "SELECT * FROM recipes WHERE recipe_name = ?",
             listOf(recipeList[position].recipeName).toTypedArray()
         )
-
         cursor.moveToFirst()
         val starred = cursor.getInt(7) == 1
         val banned = cursor.getInt(14) == 1
